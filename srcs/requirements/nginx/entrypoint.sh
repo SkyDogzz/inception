f1fef7218fe03572c7f87ed714eec1b3971e49a4
skydogzz <tstephan00@outlook.com>
2026-02-05 21:07:34 +0100

    allow only ip in env

--- srcs/requirements/nginx/entrypoint.sh
+++ srcs/requirements/nginx/entrypoint.sh
@@ -3,6 +3,24 @@ set -eu
 
 : "${DOMAIN:?DOMAIN is required}"
 
-envsubst '$DOMAIN' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+ADMINER_ALLOW_RULES=""
+adminer_ips="${ADMINER_ALLOWED_IPS:-}"
+if [ -z "$adminer_ips" ]; then
+    adminer_ips="127.0.0.1,::1"
+fi
+rules=""
+old_ifs="$IFS"
+IFS=','
+for ip in $adminer_ips; do
+    ip="$(echo "$ip" | tr -d '[:space:]')"
+    [ -z "$ip" ] && continue
+    rules="${rules}allow ${ip};\n"
+done
+IFS="$old_ifs"
+rules="${rules}deny all;"
+ADMINER_ALLOW_RULES="$(printf "%b" "$rules")"
+export ADMINER_ALLOW_RULES
+
+envsubst '$DOMAIN $ADMINER_ALLOW_RULES' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec "$@"
